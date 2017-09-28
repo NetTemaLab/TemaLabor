a467217c5152c9f79d854b5cbc8f2f16519213bc
Előd <elod@chertes.hu>
2017-09-28 16:32:27 +0200

    pelda

---
+++ DanubiusInfo.B4USApi.DTO/Reservation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanubiusInfo.B4USApi.DTO
+{
+    class Reservation
+    {
+    }
+}
